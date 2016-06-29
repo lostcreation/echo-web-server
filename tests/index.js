ab8245402378d0640322176928e5a63e2a195034
Matt McMahon <lostcreation@gmail.com>
2016-06-29 19:42:25 -0400

    Changes in progress.

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,4 @@
-const ECHO_WEB_SERVER_HOST = '0.0.0.0' // any IP, we'll actually request on localhost.
+const ECHO_WEB_SERVER_HOST = '127.0.0.1'
 const ECHO_WEB_SERVER_PORT = '2000'
 
 const serverOptions = {
@@ -8,66 +8,70 @@ const serverOptions = {
   }
 }
 
-const requestOptions = {
-  'host': 'localhost',
-  'port': ECHO_WEB_SERVER_PORT
-}
-
 const http = require('http')
 const server = require('child_process').fork('./lib/echo-web-server.js', [], serverOptions)
+server.on('close', (signal) => {
+  console.log(`child process terminated due to receipt of signal ${signal}`)
+})
+server.on('error', (err) => {
+  console.log('Failed to start web server.')
+})
 
 const tests = {
-  toRun : 0,
   run   : 0,
   pass  : 0,
   fail  : 0
 }
 
-server.on('close', (signal) => {
-  console.log(`child process terminated due to receipt of signal ${signal}`)
-})
-
-function done() {
-  tests.run++
-  if (tests.run === tests.toRun) {
-    console.log(`
-      ==============================
-      Test results:
-      ------------------------------
-      Number of Tests:   ${tests.toRun}
-      Tests Run:         ${tests.run}
-      Tests Passed:      ${tests.pass}
-      Tests Failed:      ${tests.fail}
-      ==============================
-    `)
-    server.kill()
-  }
-}
-
-function onError(e) {
-  console.log(`Problem with request: ${e.message}`);
-  tests.fail++
-  done()
+function report() {
+  console.log(`
+    ==============================
+    Test results:
+    ------------------------------
+    Tests Run:         ${tests.run}
+    Tests Passed:      ${tests.pass}
+    Tests Failed:      ${tests.fail}
+    ==============================
+  `)
 }
 
 function onSuccess(res) {
-  console.log(`STATUS: ${res.statusCode}`);
-  // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    // console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    tests.pass++
-    done()
+  return new Promise((resolve, reject) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      tests.pass++
+      resolve()
+    })
   })
 }
 
-function request(path = '/', success = onSuccess, failure = onError) {
-  let request = Object.assign({}, requestOptions, { path: encodeURI(path) })
-  http.request(request, success).on('error', failure).end();
-  tests.toRun++;
+function onFailure(e) {
+  console.log(`Problem with request: ${e.message}`);
+  tests.fail++
 }
 
-request()
-request("/<script>alert('Leet Hax!')</script>")
+function tryRequest (path = '/') {
+  const r = {
+    'path': encodeURI(path),
+    'host': ECHO_WEB_SERVER_HOST,
+    'port': ECHO_WEB_SERVER_PORT
+  }
+  return new Promise((resolve, reject) => {
+    http.request(r, resolve).on('error', reject).end();
+  })
+}
+
+tryRequest('/')
+  .then(onSuccess)
+  .catch(onFailure)
+  .then(report)
+  .then(server.kill)
+
+// tryRequest("/<script>alert('Leet Hax!')</script>")
+//   .then(onSuccess)
+//   .catch(onFailure)
