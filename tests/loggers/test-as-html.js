621c7a918d137eef4034602647aa0a35d028a149
Matt McMahon <lostcreation@gmail.com>
2016-07-04 21:38:34 -0400

    Tests for as-html module.

---
+++ tests/loggers/test-as-html.js
@@ -0,0 +1,47 @@
+const asHTML = require('../../src/loggers/as-html.js')
+const test   = require('blue-tape')
+
+function stubRes() {
+  return  { headers : {}
+          , body : ''
+          , setHeader (header, value) {
+              this.headers[header] = value
+            }
+          , write (str) {
+              this.body = this.body + str
+            }
+          }
+}
+
+test('Wellformed Request', (t) => {
+  const res = stubRes()
+  const req = { hostName: "test"
+              , port: "80"
+              , url: "/first-test"
+              , res
+              }
+  asHTML(req)
+  t.equal(res.headers['Content-Type']
+    , 'text/html'
+    , 'Should have Content-Type "text/html".'
+    )
+  t.notEqual(res.body.indexOf(`http://${req.hostName}:${req.port}${req.url}`)
+    , -1
+    , 'Should contain the requested URL in `res.body`.'
+    )
+  t.end()
+})
+
+test('Attempt to inject HTML', (t) => {
+  const res = stubRes()
+  const url = `/<script>alert("Leet Hax!")</script>`
+  asHTML({ hostName: "test"
+         , port: "80"
+         , url: url
+         , res
+         })
+  t.notEqual(res.body.indexOf("&lt;script&gt;alert(&quot;Leet Hax!&quot;)&lt;/script&gt;")
+    , -1
+    , 'Should escape HTML in the url.')
+  t.end()
+})
