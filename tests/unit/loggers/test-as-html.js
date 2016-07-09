8adf51d3011e2a99b0d8480d0968f31bbfd144fb
Matt McMahon <lostcreation@gmail.com>
2016-07-08 22:15:14 -0400

    More tests cleanup.

--- tests/unit/loggers/test-as-html.js
+++ tests/unit/loggers/test-as-html.js
@@ -6,12 +6,12 @@ const asHTML = require('../../../src/loggers/as-html.js')
 runTest('Testing safe request URI.',
   '/safe-test',
   '/safe-test',
-  'Should contain the URL inside a response tag.')
+  'Safe request URI should appear as-is inside "received" tag.')
 
 runTest('Testing unsafe request URI.',
   '/<script>alert("Leet Hax!")</script>',
   '/&lt;script&gt;alert(&quot;Leet Hax!&quot;)&lt;/script&gt;',
-  'Should escape HTML in the url.')
+  'Unsafe request URI should be escaped.')
 
 function runTest (title, inputPath, outputPath, message) {
   const response = { headers: {},
@@ -35,35 +35,38 @@ function runTest (title, inputPath, outputPath, message) {
   // Test the response for required outputs.
   test(title, (t) => {
     const body = response.body
+
     const openingTag = '<pre id="received">'
     const openingTagLocation = body.indexOf(openingTag)
-    const closingTag = '</pre>'
-    const closingTagLocation = openingTagLocation === -1 ? -1 : body.indexOf(closingTag, openingTagLocation)
     const start = openingTagLocation + openingTag.length
+
+    const closingTag = '</pre>'
+    const closingTagLocation =
+      openingTagLocation === -1 ? -1 : body.indexOf(closingTag, start)
     const end = closingTagLocation
 
-    t.equal(response.headers['Content-Type'],
-      'text/html',
-      'response "Content-Type" header should be "text/html".')
-
-    t.test('... testing response.body', (t) => {
-      t.equal(typeof body, 'string',
-        'response.body should be a string.')
-      t.notEqual(body, '',
-        'response.body should NOT be an empty string.')
-      t.notEqual(openingTagLocation, -1,
-        `${openingTag} location should be a nonnegative index.`)
-      t.equal((closingTagLocation === -1), false,
-        `${closingTag} should follow the opening tag.`)
+    t.test('Testing Response Headers.', (t) => {
+      t.equal(response.headers['Content-Type'],
+        'text/html',
+        'response "Content-Type" header should be "text/html".')
       t.end()
     })
 
-    t.test('... testing URI Parsing', (t) => {
+    t.test('Testing Response Body.', (t) => {
+      t.equal(typeof body, 'string',
+        '`response.body` should be a string.')
+      t.notEqual(body, '',
+        '`response.body` should NOT be an empty string.')
+      t.notEqual(openingTagLocation, -1,
+        `The opening tag \`${openingTag}\` should have a nonnegative index.`)
+      t.equal((closingTagLocation === -1), false,
+        `The closing tag \`${closingTag}\` should follow the opening tag.`)
       t.equal(body.substring(start, end).trim(),
         `http://${request.host}:${request.port}${outputPath}`,
         message)
       t.end()
     })
+
     t.end()
   })
 }
