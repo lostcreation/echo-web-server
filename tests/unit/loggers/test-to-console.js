45236aafb0ee44054221c4714a0c69d8a1840c80
Matt McMahon <Matt McMahon>
2016-07-09 11:13:14 -0400

    Wrong signature on test-to-console.js test.

--- tests/unit/loggers/test-to-console.js
+++ tests/unit/loggers/test-to-console.js
@@ -2,8 +2,7 @@ const test = require('blue-tape')
 const toConsole = require('../../../src/loggers/to-console.js')
 
 test('to-console module should exist.', (t) => {
-  t.equal(typeof toConsole, 'function', (t) => {
-    'toConsole should be a function.'
-  })
+  t.equal(typeof toConsole,'function',
+    'toConsole should be a function.')
   t.end()
 })
