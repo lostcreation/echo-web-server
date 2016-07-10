dc47b8243f0fc7fde7512c3c338eaac517d52ab7
Matt McMahon <Matt McMahon>
2016-07-10 12:51:42 -0400

    Standard: missing space after comma.

--- tests/unit/loggers/test-to-console.js
+++ tests/unit/loggers/test-to-console.js
@@ -1,8 +1,10 @@
+'use strict'
+
 const test = require('blue-tape')
 const toConsole = require('../../../src/loggers/to-console.js')
 
 test('to-console module should exist.', (t) => {
-  t.equal(typeof toConsole,'function',
+  t.equal(typeof toConsole, 'function',
     'toConsole should be a function.')
   t.end()
 })
