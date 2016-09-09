afb81b9827985926feb8969e7df0cc67b7265d5a
Matt McMahon <lostcreation@gmail.com>
2016-09-09 11:10:55 -0400

    Refactored addLogger to be server specific.

--- src/echo-web-server.js
+++ src/echo-web-server.js
@@ -2,33 +2,28 @@
 
 // Dependencies
 const http = require('http')
-const asHTML = require('./loggers/as-html')
-const toConsole = require('./loggers/to-console')
 
-// Exports
-module.exports = {
-  start,
-  toConsole,
-  asHTML,
-  addLogger
-}
-
-// Default Loggers
-const loggers = []
-addLogger(toConsole)
-addLogger(asHTML)
-
-/**
- * Starts a server at the port and host indicated.
- * @param {string} [port='8080'] - Open port or 'auto' to assign a random free port.
- * @param {string} [host='0.0.0.0'] - IP or host name
- * @returns {function} - Stop this server then execute an optional callback function.
- */
+// Server Config
 function start (port = '8080', host = '0.0.0.0', callback) {
   // Handle special case for an auto-port.
   if (port === 'auto') port = 0
 
-  // Create the server we're starting.
+  // Unless it has loggers, our server won't do anything.
+  const loggers = []
+
+  /**
+   * Adds a logging function. Without at least one logging function, the server
+   * will not do anything. When we recieve a request, the server will pass a
+   * `requestInfo` object to every registered logger.
+   */
+  const addLogger = loggers.push.bind(loggers)
+
+  /**
+   * Starts a server at the port and host indicated.
+   * @param {string} [port='8080'] - Open port or 'auto' to assign a random free port.
+   * @param {string} [host='0.0.0.0'] - IP or host name
+   * @returns {function} - Stop this server then execute an optional callback function.
+   */
   const server = http.createServer((req, res) => {
     const requestInfo = Object.freeze({
       res,
@@ -50,16 +45,17 @@ function start (port = '8080', host = '0.0.0.0', callback) {
     callback && callback({ host, port, running: true })
   })
 
-  return (callback) => {
-    process.disconnect && process.disconnect()
-    server.listening && server.close(callback)
+  return {
+    addLogger,
+    stop (callback) {
+      process.disconnect && process.disconnect()
+      server.listening && server.close(callback)
+    }
   }
 }
 
-/**
- * Adds a logging function that will automatically be notified of new
- * requests.
- */
-function addLogger (logger) {
-  loggers.push(logger)
+// Exports
+module.exports = {
+  start
 }
+
