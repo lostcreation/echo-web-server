cd08f6096cc521aa04f5093b1dba82cdd90ad51e
Matt McMahon <lostcreation@gmail.com>
2016-09-19 07:30:26 -0400

    Removed vestigal escaping in to-console.

--- src/loggers/to-console.js
+++ src/loggers/to-console.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = function ({req, host, port, client, url}) {
-  const clientRequest = 'http://' + (req.headers.host + url).replace(/"/g, '&quot;"')
-  console.log(`[EWS] ${host}:${port}: Client ${client} requested ${clientRequest}`)
+  console.log(`[EWS] ${host}:${port}: Client ${client} requested http://${req.headers.host}${url}`)
 }
